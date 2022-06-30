9042eaf06b8b2cdbd6429b0ff75bf1f42468b5d8
Boomexe <aptenathan@gmail.com>
2022-06-29 20:16:02 -0700

    Added bow

---
+++ src/main/java/net/boomexe/dragondrops/item/custom/DragonBowItem.java
@@ -0,0 +1,9 @@
+package net.boomexe.dragondrops.item.custom;
+
+import net.minecraft.item.BowItem;
+
+public class DragonBowItem extends BowItem {
+    public DragonBowItem(Settings settings) {
+        super(settings);
+    }
+}
